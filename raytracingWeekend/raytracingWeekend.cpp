50595a5720d302a3e206cf630e80e7c391699932
02rasjac <rasmusjacklin2002@gmail.com>
2022-04-30 19:41:59 +0200

    4.2 Add camera and render a gradient

--- raytracingWeekend/raytracingWeekend.cpp
+++ raytracingWeekend/raytracingWeekend.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
+
+/**
+* Calculate a color for the ray. When compared to rays next to each other, they form a gradient.
+* @param r A ray to get the color from.
+* @return A color for this ray. 
+*/
+color rayColor(const ray& r) {
+    vec3 unitDirection = unitVector(r.direction());
+    auto t = 0.5 * (unitDirection.y() + 1);
+    return (1.0 - t) * color(1.0) + t * color(0.5, 0.7, 1.0); // LERP the colors
+}
 
 int main() {
     // Image size
-    const int imageWidth = 256;
-    const int imageHeight = 256;
+    const double aspectRatio = 16.0 / 9.0;
+    const int imageWidth = 400;
+    const int imageHeight = static_cast<int>(imageWidth / aspectRatio);
+
+    // Camera
+    double viewportHeight = 2.0;
+    double viewportWidth = viewportHeight * aspectRatio;
+    double focalLength = 1.0;
+
+    point3 origin = point3(0);
+    vec3 horizontal = vec3(viewportWidth, 0, 0);
+    vec3 vertical = vec3(0, viewportHeight, 0);
+    point3 lowerLeftCorner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focalLength);
 
     // Renderer
     std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n"; // Colors in ASCII << Imagesize << Maxcolor 255
@@ -14,7 +37,10 @@ int main() {
     for (int j = imageHeight-1; j >= 0; j--) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < imageWidth; i++) {
-            color pixelColor(double(i) / (imageWidth - 1), double(j) / (imageHeight - 1), 0.25);
+            double u = double(i) / (imageWidth - 1);
+            double v = double(j) / (imageHeight - 1);
+            ray r(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
+            color pixelColor = rayColor(r);
             writeColor(std::cout, pixelColor);
         }
     }

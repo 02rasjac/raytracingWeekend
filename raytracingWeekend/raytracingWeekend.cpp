596b34d70713df81ed0faffd468b6271d573e43c
02rasjac <rasmusjacklin2002@gmail.com>
2022-05-05 18:22:37 +0200

    8.4 Add tolerance on reflection (fix shadow acne)

--- raytracingWeekend/raytracingWeekend.cpp
+++ raytracingWeekend/raytracingWeekend.cpp
@@ -16,7 +16,7 @@ color rayColor(const ray& r, const hittable& world, int depth) {
     // Return black if too deep
     if (depth <= 0) return color(0);
 
-    if (world.hit(r, 0, infinity, rec)) {
+    if (world.hit(r, 0.001, infinity, rec)) {
         point3 target = rec.p + rec.normal + randomInUnitSphere();
         return 0.5 * rayColor(ray(rec.p, target - rec.p), world, --depth);
     }
